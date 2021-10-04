17b1354d4a5cf4366365bbe14eaa37f49a76ef48
Juscelino <juscelinodjj@gmail.com>
2021-10-04 17:02:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,78 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express();
-
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const mongoUri = process.env.MONGO_URI;
 
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const urlSchema = new Schema({
+  id: { type: Number, required: true },
+  url: { type: String, required: true }
+});
+
+const urls = mongoose.model('urls', urlSchema);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:id?', (req, res) => {
+  const id = req.params.id;
+  const wrongFormat = id.match(/^[\d]+$/) === null;
+  if (wrongFormat) {
+    return res.json({'error': 'Wrong format'});
+  }
+  urls.find({id: id}, (error, urlFound) => {
+    if (error) {
+      return console.log(error);
+    }
+    if (!urlFound[0]) {
+      return res.json({'error':	'No short URL found for the given input'});
+    }
+    const {url} = urlFound[0];
+    res.redirect(url);
+  });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', (req, res) => {
+  const url = req['body']['url'];
+  request(url, (error, response, body) => {
+    const urlExist = response && response.statusCode === 200 ? true : false;
+    if (!urlExist) {
+      return res.json({'error': 'Invalid URL'});
+    }
+    urls.count({}, (error, count) => {
+      if (error) {
+        return console.error(error);
+      }
+      const id = count + 1;
+      const newUrl = new urls({id, url});
+      newUrl.save((error, data) => {
+        if (error) {
+          return console.error(error);
+        }
+        res.json({
+          'original_url': url,
+          'short_url': id
+        });
+      });
+    });
+  });
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
